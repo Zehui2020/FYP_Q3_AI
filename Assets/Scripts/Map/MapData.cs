9065d5084b527c27fddefcc63df0a202d55ab7cd
K0d3y <125231413+K0d3y@users.noreply.github.com>
2024-08-27 11:25:38 +0800

    Added basic procedural map generation

---
+++ Assets/Scripts/Map/MapData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class MapData : ScriptableObject
+{
+    public int m_width, m_height;
+}
---
+++ Assets/Scripts/Map/MapGenerator.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MapGenerator : MonoBehaviour
+{
+    [SerializeField] private int noOfRooms = 10;
+    [SerializeField] private List<GameObject> roomPrefabs;
+    [SerializeField] private GameObject mapContainer;
+
+    private List<RoomData> roomData = new List<RoomData>();
+    private List<GameObject> roomList = new List<GameObject>();
+    private List<Vector2> spaceTaken = new List<Vector2>();
+    private List<Vector2> directions = new List<Vector2>
+        {
+            new Vector2(0, 1),
+            new Vector2(0, -1),
+            new Vector2(1, 0),
+            new Vector2(-1, 0)
+        };
+
+    private float temp = 0;
+
+    private void Start()
+    {
+        SpawnRooms();
+    }
+
+    private void Update()
+    {
+        // debug inputs
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            SpawnRooms();
+        }
+
+        if (temp < 1)
+        {
+            temp += Time.deltaTime;
+        }
+        else
+        {
+            SpawnRooms();
+            temp = 0;
+        }
+    }
+
+    private GameObject GetRandomRoom()
+    {
+        // add random room to the list of rooms
+        GameObject obj = roomPrefabs[Random.Range(0, roomPrefabs.Count)];
+        roomData.Add(obj.GetComponent<RoomData>());
+        return obj;
+    }
+
+    private void SpawnRooms()
+    {
+        // clear map
+        foreach (GameObject room in roomList)
+        {
+            Destroy(room);
+        }
+        roomList.Clear();
+        roomData.Clear();
+        spaceTaken.Clear();
+        Vector2 roomPos = Vector2.zero;
+        // get random room and place it
+        GameObject roomToSpawn = GetRandomRoom();
+        GameObject obj = Instantiate(roomPrefabs[0]);
+        obj.transform.SetParent(mapContainer.transform);
+        obj.transform.position = roomPos;
+        spaceTaken.Add(Vector2.zero);
+        roomList.Add(obj);
+        // spawn other rooms
+        for (int i = 1; i < noOfRooms; i++)
+        {
+            roomToSpawn = GetRandomRoom();
+            Vector2 dir = GetRandomDirection(0);
+            obj = Instantiate(roomToSpawn);
+            obj.transform.SetParent(mapContainer.transform);
+            obj.transform.position = roomPos + (dir * (roomData[i].roomSize + roomData[i - 1].roomSize));
+            spaceTaken.Add(spaceTaken[spaceTaken.Count - 1] + dir);
+            roomList.Add(obj);
+            roomPos = obj.transform.position;
+        }
+    }
+
+    private Vector2 GetRandomDirection(int num)
+    {
+        int i = Random.Range(0, directions.Count - num);
+        Vector2 dir = directions[i];
+        directions.RemoveAt(i);
+        directions.Add(dir);
+        if (spaceTaken.Contains(spaceTaken[spaceTaken.Count - 1] + dir))
+        {
+            dir = GetRandomDirection(num++);
+        }
+        return dir;
+    }
+}
