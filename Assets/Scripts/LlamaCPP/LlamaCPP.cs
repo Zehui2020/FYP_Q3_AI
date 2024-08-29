48f94955ef745d326ef59ad2cd72ec3e96ca969c
RT-ZK <reamustan06@gmail.com>
2024-08-29 16:37:33 +0800

    Changed the Text Gen code to be more compatible with other devices.

--- Assets/Scripts/LlamaCPP/LlamaCPP.cs
+++ Assets/Scripts/LlamaCPP/LlamaCPP.cs
@@ -12,9 +12,9 @@ public class LlamaCPP : MonoBehaviour
     public TMP_InputField user_Input;
 
     private string userPrompt;
-    
-    private string llamaDirectory = @"C:\Users\rtzk2\llama.cpp";
-    private string modelDirectory = @"C:\Users\rtzk2\llama.cpp\models\Trail_3\llama-2-7b-chat.Q4_K_M.gguf";
+
+    private string llamaDirectory = @"C:\llama.cpp";
+    private string modelDirectory = @"C:\llama.cpp\models\Trail_3\llama-2-7b-chat.Q4_K_M.gguf";
     private string AI_Gen_Prompt;
 
     void Start()
@@ -33,7 +33,19 @@ public class LlamaCPP : MonoBehaviour
         if (!string.IsNullOrEmpty(user_Input.text))
         {
             userPrompt = user_Input.text;
-            AI_Gen_Prompt = '"' + "[INST] <<SYS>> You are a writer and your primary job is to write concise descriptions for game items. In this environment, do not address the user and do not show XML tags other than these ones below: <result></result> Here are a few examples of what your output should look like: <result>This is a sacred sword from times of old, held by a warrior named Link. With this sword, he has fell many dragons and the mighty Ganandolf.</result> <result>A magical sword wielded by evil warriors known as the Sith. It consists of a plasma blade, powered by a kyber crystal. The sound of its hum in a silent room signals the beginning of the end of your life.</result> Here is a request to write a description for a game item: <</SYS>> {" + userPrompt + "} [/INST]" + '"';
+            AI_Gen_Prompt = 
+                '"' + 
+                "[INST] <<SYS>> You are a writer and your primary job is to write concise descriptions for game items. " +
+                "In this environment, do not address the user and do not show XML tags other than these ones below: <result></result> " +
+                
+                "Here are a few examples of what your output should look like: " +
+                "<result>This is a sacred sword from times of old, held by a warrior named Link. " +
+                "With this sword, he has fell many dragons and the mighty Ganandolf.</result> " +
+
+                "<result>A magical sword wielded by evil warriors known as the Sith. " +
+                "It consists of a plasma blade, powered by a kyber crystal. " +
+                "The sound of its hum in a silent room signals the beginning of the end of your life.</result> " +
+                "Here is a request to write a description for a game item: <</SYS>> {" + userPrompt + "} [/INST]" + '"';
             string fullCommand = $"cd {llamaDirectory} && llama-cli -m {modelDirectory} --no-display-prompt -p {AI_Gen_Prompt}";
             OpenCommandPrompt(fullCommand);
         }
