2dfae7f09126ac764f2b4ab639cce4732b2a5cd9
Zehui2020 <123875605+Zehui2020@users.noreply.github.com>
2024-09-03 14:23:40 +0800

    Started work on AI tile swapping

--- Assets/Scripts/ImageGen/TilemapManager.cs
+++ Assets/Scripts/ImageGen/TilemapManager.cs
@@ -5,12 +5,38 @@ using UnityEngine.Tilemaps;
 
 public class TilemapManager : MonoBehaviour
 {
-    [SerializeField] private Tilemap tilemap;
-    [SerializeField] private TileBase newTile;
-    [SerializeField] private TileBase targetTile;
+    [Header("Tile Slicing")]
+    [SerializeField] private ImageSaver imageSaver;
+    [SerializeField] private string fileName;
+    [SerializeField] private List<Rect> tileRects;
+    private List<Sprite> slicedSprites = new();
 
-    public void SwapTile()
+    [Header("Tilemap Manipulation")]
+    [SerializeField] private Tilemap tilemap;
+    [SerializeField] private List<TileBase> targetTiles;
+
+    public void SliceTexture()
     {
-        tilemap.SwapTile(newTile, targetTile);
+        if (targetTiles.Count != tileRects.Count)
+        {
+            Debug.LogError("Insufficient Tiles To Set!");
+            return;
+        }
+
+        Texture2D texture = imageSaver.GetTextureFromLocalDisk(fileName);
+
+        foreach (Rect rect in tileRects)
+        {
+            Sprite newSprite = Sprite.Create(texture, rect, new Vector2(0.5f, 0.5f));
+            slicedSprites.Add(newSprite);
+        }
+    }
+
+    public void AssignTileSprites()
+    {
+        for (int i = 0; i < targetTiles.Count; i++)
+        {
+
+        }
     }
 }
--- Assets/Scripts/ImageSaver.cs
+++ Assets/Scripts/ImageSaver.cs
@@ -31,4 +31,18 @@ public class ImageSaver : MonoBehaviour
         loadTexture.LoadImage(textureBytes);
         return Sprite.Create(loadTexture, new Rect(0f, 0f, loadTexture.width, loadTexture.height), new Vector2(0.5f, 0.5f));
     }
+
+    public Texture2D GetTextureFromLocalDisk(string fileName)
+    {
+        if (!File.Exists(Application.persistentDataPath + "_" + fileName))
+        {
+            Debug.Log("File does not exist on local disk!");
+            return null;
+        }
+
+        byte[] textureBytes = File.ReadAllBytes(Application.persistentDataPath + "_" + fileName);
+        Texture2D loadTexture = new Texture2D(0, 0);
+        loadTexture.LoadImage(textureBytes);
+        return loadTexture;
+    }
 }
