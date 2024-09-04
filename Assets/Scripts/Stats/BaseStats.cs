6580fc4dce4fccf7878b2aebcc4c460ddc906d19
K0d3y <125231413+K0d3y@users.noreply.github.com>
2024-09-04 14:24:26 +0800

    Changed some code

--- Assets/Scripts/Stats/BaseStats.cs
+++ Assets/Scripts/Stats/BaseStats.cs
@@ -110,16 +110,15 @@ public class BaseStats : MonoBehaviour
             else if (effectType == BaseAbility.AbilityEffectType.Decrease)
                 change = -(int)(value * baseAttack / 100);
         }
-        attack += (int)change;
+        attack += change;
 
         yield return new WaitForSeconds(duration);
 
-        attack -= (int)change;
+        attack -= change;
     }
 
     public IEnumerator HealthChangeRoutine(float value, BaseAbility.AbilityEffectType effectType, BaseAbility.AbilityEffectValueType valueType,  float duration)
     {
-        Debug.Log("health change start");
         int change = 0;
         if (valueType == BaseAbility.AbilityEffectValueType.Flat)
         {
@@ -135,19 +134,15 @@ public class BaseStats : MonoBehaviour
             else if (effectType == BaseAbility.AbilityEffectType.Decrease)
                 change = -(int)(value * maxHealth / 100);
         }
-        Debug.Log(change);
         int temp = health + change;
         temp = Mathf.Clamp(temp, 0, maxHealth);
         change = temp - health;
         health += change;
-        Debug.Log(change);
 
         DamagePopup popup = ObjectPool.Instance.GetPooledObject("DamagePopup", true) as DamagePopup;
         popup.SetupPopup("+ " + change, transform.position, Color.green);
 
         yield return new WaitForSeconds(duration);
-
-        Debug.Log("health change end");
 
         if (duration > 0)
         {
