da7e7f84f8ff343a1cb936bab80a461c4ae02b4f
K0d3y <125231413+K0d3y@users.noreply.github.com>
2024-11-04 16:59:29 +0800

    Added lighting for abilities

--- Assets/Scripts/Combat/Abilities/AbilityAnimationController.cs
+++ Assets/Scripts/Combat/Abilities/AbilityAnimationController.cs
@@ -1,8 +1,18 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using System.Collections.Generic;
+using System.Collections;
 
 public class AbilityAnimationController : MonoBehaviour
 {
     [SerializeField] public Animator abilityOverlayAnimator;
+    [SerializeField] private List<Light2D> spotLight;
+
+    private void Awake()
+    {
+        for (int i = 0; i < spotLight.Count; i++)
+            spotLight[i].enabled = false;
+    }
 
     public void TriggerOverlayAnim(float alpha, string trigger)
     {
@@ -14,6 +24,48 @@ public class AbilityAnimationController : MonoBehaviour
     {
         bool show = i == 0 ? false : true;
         abilityOverlayAnimator.GetComponent<SpriteRenderer>().enabled = show;
+    }
+
+    public void ShowAbilityLight(int i)
+    {
+        bool show = !spotLight[i].enabled;
+        StartCoroutine(LightRoutine(i, show));
+    }
+
+    private IEnumerator LightRoutine(int i, bool show)
+    {
+        float timer = 2;
+        float a;
+
+        if (show)
+        {
+            a = 0;
+            spotLight[i].enabled = show;
+        }
+        else
+            a = 1;
+
+        spotLight[i].color = new Color(spotLight[i].color.r, spotLight[i].color.g, spotLight[i].color.b, a);
+
+        while (timer > 0)
+        {
+            if (show)
+                a = Mathf.Lerp(a, 1, 0.05f);
+            else
+                a = Mathf.Lerp(a, 0, 0.05f);
+
+            spotLight[i].color = new Color(spotLight[i].color.r, spotLight[i].color.g, spotLight[i].color.b, a);
+
+            timer -= Time.deltaTime;
+            yield return null;
+        }
+
+        spotLight[i].enabled = show;
+
+        if (!show)
+            spotLight[i].color = new Color(spotLight[i].color.r, spotLight[i].color.g, spotLight[i].color.b, 0);
+        else
+            spotLight[i].color = new Color(spotLight[i].color.r, spotLight[i].color.g, spotLight[i].color.b, 1);
     }
 
     public void ShowPlayer(int i)
