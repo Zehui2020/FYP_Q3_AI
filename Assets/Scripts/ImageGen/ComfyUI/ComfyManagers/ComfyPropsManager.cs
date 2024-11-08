cbe5e6263b40f31fd0beefd7c100344de4127b3e
Zehui2020 <123875605+Zehui2020@users.noreply.github.com>
2024-11-08 10:44:28 +0800

    Finished up prop generation

--- Assets/Scripts/ImageGen/ComfyUI/ComfyManagers/ComfyPropsManager.cs
+++ Assets/Scripts/ImageGen/ComfyUI/ComfyManagers/ComfyPropsManager.cs
@@ -13,25 +13,38 @@ public class ComfyPropsManager : ComfyManager
 
     [SerializeField] private int propsRecieved = 0;
     [SerializeField] private List<PropData> propDatas;
+    [SerializeField] private bool queueOnStart;
+
+    private void Start()
+    {
+        InitManager();
+
+        propsRecieved = 0;
+        if (queueOnStart)
+            QueuePropPrompt();
+    }
 
     public void QueuePropPrompt()
     {
         if (propsRecieved >= propDatas.Count)
             return;
 
-        GameData.Instance.currentlyLoadingImage.Enqueue(propDatas[propsRecieved] + "_Prop");
+        GameData.Instance.currentlyLoadingImage.Enqueue(propDatas[propsRecieved].fileName + "_Prop");
         promptCtr.QueuePromptWithControlNet(propDatas[propsRecieved].prompt, propDatas[propsRecieved].referenceImage);
     }
 
     public override bool OnRecieveImage(string promptID, Texture2D texture)
     {
+        Debug.Log(propsRecieved);
+        Debug.Log(propDatas.Count);
+
         if (propsRecieved >= propDatas.Count)
         {
             Destroy(gameObject);
             return false;
         }
 
-        fileName = propDatas[propsRecieved].prompt;
+        fileName = propDatas[propsRecieved].fileName;
         Debug.Log("RECIEVED " + fileName);
 
         if (base.OnRecieveImage(promptID, texture))
