9efd99d9dcbd105170b472520dd16c3c1a7a4ed8
K0d3y <125231413+K0d3y@users.noreply.github.com>
2024-11-08 14:25:20 +0800

    Added door sfx

--- Assets/Scripts/Interactables/Door.cs
+++ Assets/Scripts/Interactables/Door.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Door : MonoBehaviour, IInteractable
@@ -5,15 +6,24 @@ public class Door : MonoBehaviour, IInteractable
     [SerializeField] private SimpleAnimation keycodeUI;
     [SerializeField] private Animator animator;
     [SerializeField] private string nextLevel;
+    [SerializeField] private List<AudioSource> audioSources = new();
+    private bool isActivated = false;
 
     public void OnEnterRange()
     {
         keycodeUI.Show();
         animator.SetTrigger("activate");
+
+        if (!isActivated)
+            foreach (AudioSource source in audioSources)
+                source.Play();
+
+        isActivated = true;
     }
 
     public bool OnInteract()
     {
+        AudioManager.Instance.PlayOneShot(Sound.SoundName.TeleportStart);
         SceneLoader.Instance.LoadScene(nextLevel);
 
         keycodeUI.Hide();
