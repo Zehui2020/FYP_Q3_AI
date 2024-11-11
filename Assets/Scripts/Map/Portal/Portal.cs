d7bc1e68c988dc556b078a2cea2b64e5c10c49d4
Zehui2020 <123875605+Zehui2020@users.noreply.github.com>
2024-11-11 18:45:41 +0800

    Added glow to teleport

--- Assets/Scripts/Map/Portal/Portal.cs
+++ Assets/Scripts/Map/Portal/Portal.cs
@@ -1,12 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
 
 public class Portal : MonoBehaviour, IInteractable
 {
     [SerializeField] private Animator animator;
     [SerializeField] private AudioSource audioSource;
+    [SerializeField] private List<Light2D> glow = new();
     public bool isActivated = false;
     public GameObject button;
+
+    private void Start()
+    {
+        foreach (Light2D light in glow)
+            light.intensity = 0f;
+    }
 
     public void OnEnterRange()
     {
@@ -33,5 +43,26 @@ public class Portal : MonoBehaviour, IInteractable
         audioSource.Play();
         isActivated = true;
         button.SetActive(true);
+        StartCoroutine(ActivateGlow());
+    }
+
+    private IEnumerator ActivateGlow()
+    {
+        float duration = 1f;
+        float timeElapsed = 0f;
+
+        while (timeElapsed < duration)
+        {
+            timeElapsed += Time.deltaTime;
+            float currentIntensity = Mathf.Lerp(0, 1f, timeElapsed / duration);
+
+            foreach (Light2D light in glow)
+                light.intensity = currentIntensity;
+
+            yield return null;
+        }
+
+        foreach (Light2D light in glow)
+            light.intensity = 1f;
     }
 }
